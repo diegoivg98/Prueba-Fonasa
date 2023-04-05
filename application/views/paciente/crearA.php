197b4294a9856038f450767a1eb907a5b38ee293
Diego Vistoso <diegoivg98@gmail.com>
2023-04-05 18:32:55 -0400

    arreglo en redondear calculo de prioridad a 2 decimales

--- application/views/paciente/crearA.php
+++ application/views/paciente/crearA.php
@@ -56,8 +56,8 @@
 								<div class="mb-3">
 									<label for="">¿TIENE DIETA?:</label>
 									<select name="tiene_dieta" id="tiene_dieta" class="form-control">
-										<option value="1">SI</option>
-										<option value="0">NO</option>
+										<option value="si">SI</option>
+										<option value="no">NO</option>
 									</select>
 								</div>
 
@@ -75,6 +75,52 @@
 
 								<button type="submit" class="btn btn-primary">INGRESAR</button>
 							</form>
+							<script>
+								const edadInput = document.getElementById('edad');
+								const dietaInput = document.getElementById('tiene_dieta');
+								const prioridadInput = document.getElementById('prioridad');
+								const riesgoInput = document.getElementById('riesgo');
+
+								function calcularPrioridad() {
+									const edad = parseInt(edadInput.value);
+									const tieneDieta = dietaInput.value === 'si';
+
+									if (tieneDieta) {
+										if (edad < 60 || edad > 100) {
+											alert('Si tiene dieta, la edad debe estar entre 60 y 100 años');
+											prioridadInput.value = '';
+											riesgoInput.value = '';
+											document.getElementById('submitBtn').disabled = true;
+											return;
+										}
+									}
+
+									let prioridad = 0;
+									if (tieneDieta && edad >= 60 && edad <= 100) {
+										prioridad = (edad / 20) + 4;
+									} else {
+										prioridad = (edad / 30) + 3;
+									}
+
+									// redondea el valor de prioridad a 2 decimales
+									prioridadInput.value = prioridad.toFixed(2);
+
+									calcularRiesgo();
+								}
+
+
+
+								function calcularRiesgo() {
+									const edad = parseInt(edadInput.value);
+									const prioridad = parseInt(prioridadInput.value);
+
+									riesgoInput.value = (edad * prioridad) / 100 + 5.3;
+								}
+
+								edadInput.addEventListener('change', calcularPrioridad);
+								dietaInput.addEventListener('change', calcularPrioridad);
+							</script>
+
 						</div>
 						<div class="card-footer text-muted">
 							Footer
