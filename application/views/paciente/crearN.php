b5e0da507efa818c7770177b71bef135ae3c12b9
Diego Vistoso <diegoivg98@gmail.com>
2023-04-05 16:54:56 -0400

    cambio menor en formulario agregar paciente niño

--- application/views/paciente/crearN.php
+++ application/views/paciente/crearN.php
@@ -26,7 +26,7 @@
 				<div class="col-md-12">
 					<div class="card">
 						<div class="card-header">
-							FORMULARIO Paciente
+							FORMULARIO PACIENTE NIÑO
 						</div>
 						<div class="card-body">
 							<?php if ($this->session->flashdata("error")) : ?>
