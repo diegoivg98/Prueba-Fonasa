ccabee95327bccf390b758c76e0797054205c539
Diego Vistoso <diegoivg98@gmail.com>
2023-04-07 21:08:12 -0400

    agregar mismo estilo Bootstrap 5 a formulario de registro de pacientes niño, joven y adulto

--- application/views/paciente/crearA.php
+++ application/views/paciente/crearA.php
@@ -1,69 +1,66 @@
-<div class="content-wrapper">
-	<section class="content">
-		<div class="container-fluid">
-			<div class="card card-default">
-				<br>
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header">
-							FORMULARIO PACIENTE ADULTO
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					FORMULARIO PACIENTE ADULTO
+				</div>
+				<div class="card-body">
+					<?php if ($this->session->flashdata("error")) : ?>
+						<div class="alert alert-danger alert-dismissible">
+							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+							<p><i class="icon fa fa-ban"></i><?php echo $this->session->flashdata("error"); ?></p>
 						</div>
-						<div class="card-body">
-							<?php if ($this->session->flashdata("error")) : ?>
-								<div class="alert alert-danger alert-dismissible">
-									<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-									<p><i class="icon fa fa-ban"></i><?php echo $this->session->flashdata("error"); ?></p>
-								</div>
-							<?php endif; ?>
-							<form action=" <?php echo base_url(); ?>pacientes/storeanciano" method="post">
-								<div class="mb-3">
-									<label for="" class="form-label">NOMBRE</label>
-									<input onkeypress="return sololetras(event)" placeholder="Ingrese nombre del paciente" type="text" id="nombre" name="nombre" class="form-control">
-									<?php echo form_error("nombre", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="" class="form-label">EDAD (41-100)</label>
-									<input min="41" max="100" placeholder="Ingrese edad del paciente" type="number" id="edad" name="edad" class="form-control">
-									<?php echo form_error("edad", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="">NUM HISTORIA CLINICA</label>
-									<input readonly name="num_historiaclinica" id="num_historiaclinica" maxlength="16" size="16" class="form-control">
-								</div>
-
-								<div class="mb-3">
-									<label for="">¿TIENE DIETA?:</label>
-									<select name="tiene_dieta" id="tiene_dieta" class="form-control">
-										<option value="si">SI</option>
-										<option value="no">NO</option>
-									</select>
-								</div>
-
-								<div class="mb-3">
-									<label for="">PRIORIDAD</label>
-									<input readonly type="number" name="prioridad" id="prioridad" class="form-control">
-									<?php echo form_error("prioridad", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="">RIESGO</label>
-									<input readonly type="number" name="riesgo" id="riesgo" class="form-control">
-									<?php echo form_error("riesgo", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<button type="submit" class="btn btn-primary">INGRESAR</button>
-							</form>
+					<?php endif; ?>
+					<form action=" <?php echo base_url(); ?>pacientes/storeanciano" method="post">
+						<div class="mb-3">
+							<label for="" class="form-label">NOMBRE</label>
+							<input onkeypress="return sololetras(event)" placeholder="Ingrese nombre del paciente" type="text" id="nombre" name="nombre" class="form-control">
+							<?php echo form_error("nombre", "<span class='help-block'>", "</span>"); ?>
 						</div>
-						<div class="card-footer text-muted">
-							Footer
+
+						<div class="mb-3">
+							<label for="" class="form-label">EDAD (41-100)</label>
+							<input min="41" max="100" placeholder="Ingrese edad del paciente" type="number" id="edad" name="edad" class="form-control">
+							<?php echo form_error("edad", "<span class='help-block'>", "</span>"); ?>
 						</div>
-					</div>
+
+						<div class="mb-3">
+							<label for="">NUM HISTORIA CLINICA</label>
+							<input readonly name="num_historiaclinica" id="num_historiaclinica" maxlength="16" size="16" class="form-control">
+						</div>
+
+						<div class="mb-3">
+							<label for="">¿TIENE DIETA?:</label>
+							<select name="tiene_dieta" id="tiene_dieta" class="form-control">
+								<option value="si">SI</option>
+								<option value="no">NO</option>
+							</select>
+						</div>
+
+						<div class="mb-3">
+							<label for="">PRIORIDAD</label>
+							<input readonly type="number" name="prioridad" id="prioridad" class="form-control">
+							<?php echo form_error("prioridad", "<span class='help-block'>", "</span>"); ?>
+						</div>
+
+						<div class="mb-3">
+							<label for="">RIESGO</label>
+							<input readonly type="number" name="riesgo" id="riesgo" class="form-control">
+							<?php echo form_error("riesgo", "<span class='help-block'>", "</span>"); ?>
+						</div>
+
+						<button type="submit" class="btn btn-primary">INGRESAR</button>
+					</form>
+				</div>
+				<div class="card-footer text-muted">
+					Footer
 				</div>
 			</div>
 		</div>
+	</div>
+</div>
 </div>
 
-<script src="<?=base_url()?>application/views/js/randomNumber.js" type="text/javascript" charset="utf-8"></script>
-<script src="<?=base_url()?>application/views/js/calculoadulto.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?= base_url() ?>application/views/js/randomNumber.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?= base_url() ?>application/views/js/calculoadulto.js" type="text/javascript" charset="utf-8"></script>
--- application/views/paciente/crearJ.php
+++ application/views/paciente/crearJ.php
@@ -1,84 +1,75 @@
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-	<!-- Main content -->
-	<section class="content">
-		<div class="container-fluid">
-			<!-- SELECT2 EXAMPLE -->
-			<div class="card card-default">
-
-				<!-- /.card-header -->
-				<!-- /.card-body -->
-				<br>
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header">
-							FORMULARIO PACIENTE JOVEN
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					FORMULARIO PACIENTE JOVEN
+				</div>
+				<div class="card-body">
+					<?php if ($this->session->flashdata("error")) : ?>
+						<div class="alert alert-danger alert-dismissible">
+							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+							<p><i class="icon fa fa-ban"></i><?php echo $this->session->flashdata("error"); ?></p>
 						</div>
-						<div class="card-body">
-							<?php if ($this->session->flashdata("error")) : ?>
-								<div class="alert alert-danger alert-dismissible">
-									<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-									<p><i class="icon fa fa-ban"></i><?php echo $this->session->flashdata("error"); ?></p>
-								</div>
-							<?php endif; ?>
-							<form action=" <?php echo base_url(); ?>pacientes/storejoven" method="post">
+					<?php endif; ?>
+					<form action=" <?php echo base_url(); ?>pacientes/storejoven" method="post">
 
-								<div class="mb-3">
-									<label for="" class="form-label">NOMBRE</label>
-									<input required onkeypress="return sololetras(event)" placeholder="Ingrese nombre del paciente" type="text" id="nombre" name="nombre" class="form-control">
-									<?php echo form_error("nombre", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="" class="form-label">EDAD (16-40)</label>
-									<input min="16" max="40" placeholder="Ingrese edad del paciente" type="number" id="edad" name="edad" class="form-control" required>
-									<?php echo form_error("edad", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="" class="form-label">NUMERO HISTORIA CLINICA</label>
-									<input readonly name="num_historiaclinica" id="num_historiaclinica" maxlength="16" size="16" class="form-control">
-								</div>
-
-								<div class="mb-3">
-									<label for="Pefil">¿ES FUMADOR?:</label>
-									<select name="fumador" id="fumador" class="form-control">
-										<option value="si">SI</option>
-										<option value="no">NO</option>
-									</select>
-								</div>
-
-								<div class="mb-3">
-									<label for="" class="form-label">AÑOS FUMADOR</label>
-									<input min="1" placeholder="Ingrese cuantos años lleva fumando" type="number" id="años_fumador" name="años_fumador" class="form-control">
-									<?php echo form_error("años_fumador", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="">PRIORIDAD</label>
-									<input readonly type="number" name="prioridad" id="prioridad" class="form-control" required>
-									<?php echo form_error("prioridad", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<div class="mb-3">
-									<label for="">RIESGO</label>
-									<input readonly type="number" name="riesgo" id="riesgo" class="form-control" required>
-									<?php echo form_error("riesgo", "<span class='help-block'>", "</span>"); ?>
-								</div>
-
-								<button type="submit" class="btn btn-primary">INGRESAR</button>
-							</form>
+						<div class="mb-3">
+							<label for="" class="form-label">NOMBRE</label>
+							<input required onkeypress="return sololetras(event)" placeholder="Ingrese nombre del paciente" type="text" id="nombre" name="nombre" class="form-control">
+							<?php echo form_error("nombre", "<span class='help-block'>", "</span>"); ?>
 						</div>
-						<div class="card-footer text-muted">
-							Footer
+
+						<div class="mb-3">
+							<label for="" class="form-label">EDAD (16-40)</label>
+							<input min="16" max="40" placeholder="Ingrese edad del paciente" type="number" id="edad" name="edad" class="form-control" required>
+							<?php echo form_error("edad", "<span class='help-block'>", "</span>"); ?>
 						</div>
-					</div>
+
+						<div class="mb-3">
+							<label for="" class="form-label">NUMERO HISTORIA CLINICA</label>
+							<input readonly name="num_historiaclinica" id="num_historiaclinica" maxlength="16" size="16" class="form-control">
+						</div>
+
+						<div class="mb-3">
+							<label for="Pefil">¿ES FUMADOR?:</label>
+							<select name="fumador" id="fumador" class="form-control">
+								<option value="si">SI</option>
+								<option value="no">NO</option>
+							</select>
+						</div>
+
+						<div class="mb-3">
+							<label for="" class="form-label">AÑOS FUMADOR</label>
+							<input min="1" placeholder="Ingrese cuantos años lleva fumando" type="number" id="años_fumador" name="años_fumador" class="form-control">
+							<?php echo form_error("años_fumador", "<span class='help-block'>", "</span>"); ?>
+						</div>
+
+						<div class="mb-3">
+							<label for="">PRIORIDAD</label>
+							<input readonly type="number" name="prioridad" id="prioridad" class="form-control" required>
+							<?php echo form_error("prioridad", "<span class='help-block'>", "</span>"); ?>
+						</div>
+
+						<div class="mb-3">
+							<label for="">RIESGO</label>
+							<input readonly type="number" name="riesgo" id="riesgo" class="form-control" required>
+							<?php echo form_error("riesgo", "<span class='help-block'>", "</span>"); ?>
+						</div>
+
+						<button type="submit" class="btn btn-primary">INGRESAR</button>
+					</form>
+				</div>
+				<div class="card-footer text-muted">
+					Footer
 				</div>
 			</div>
 		</div>
-		<!-- /.card -->
+	</div>
+</div>
+<!-- /.card -->
 </div>
 <!-- /.content-wrapper -->
 
-<script src="<?=base_url()?>application/views/js/randomNumber.js" type="text/javascript" charset="utf-8"></script>
-<script src="<?=base_url()?>application/views/js/calculojoven.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?= base_url() ?>application/views/js/randomNumber.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?= base_url() ?>application/views/js/calculojoven.js" type="text/javascript" charset="utf-8"></script>
